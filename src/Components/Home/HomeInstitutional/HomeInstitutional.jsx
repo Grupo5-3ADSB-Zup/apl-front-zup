78b138a70e9d0f3ad49078466998e91e41874ae6
MatheusSs17 <matheus.ssantos@bandtec.com.br>
2023-04-07 23:35:33 -0300

    front2

--- src/Components/Home/HomeInstitutional/HomeInstitutional.jsx
+++ src/Components/Home/HomeInstitutional/HomeInstitutional.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './HomeInstitutional.css';
-
+import icon from '../../../assets/images/icon.png'
+import logo from '../../../assets/images/zupMarelo4.png'
+import banner from '../../../assets/images/zupMarelo1.png'
 
 
 class HomeInstitutional extends Component {
@@ -8,7 +10,7 @@ class HomeInstitutional extends Component {
         return (
             <div className='page'>
                 <div className='nav'>
-                    <div></div>
+                <img src={logo}/>
                     <div className='form'>
                         <button >LOGIN</button>
                         <button >CADASTRO</button>
@@ -27,11 +29,48 @@ class HomeInstitutional extends Component {
                     </div>
                 </div>
                 <div className='about-us'>
-                    <h1>Plataforma feita por investidores para investidores</h1>
+                    <h1>Plataforma feita por investidores
+                        para investidores</h1>
                     <div className='about-us-labels'>
-                        <div className='about-us-divs'>aaa</div>
-                        <div className='about-us-divs'>aaa</div>
-                        <div className='about-us-divs'>aaa</div>
+                        
+                        <div className='about-us-divs'>
+                        <div className='about-us-title'>
+                        <img src={icon}/>
+                                <h2>Noticias Precisas</h2>
+                            </div>
+                        <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, odit,
+                             enim consequuntur ex quam nam rerum quos architecto perspiciatis tempore eaque impedit maxime,
+                             at nesciunt voluptatem aperiam repellendus minima quae!</label>
+                        </div>
+                        <div className='about-us-divs'>
+                        <div className='about-us-title'>
+                        <img src={icon} alt="" />
+                                <h2>Influenciadores</h2>
+                            </div>
+                        <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, odit,
+                             enim consequuntur ex quam nam rerum quos architecto perspiciatis tempore eaque impedit maxime,
+                             at nesciunt voluptatem aperiam repellendus minima quae!</label>
+                        </div>
+                        <div className='about-us-divs'>
+                        <div className='about-us-title'>
+                            <img src={icon} alt="" />
+                                <h2>Interação entre usuários</h2>
+                            </div>
+                        <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, odit,
+                             enim consequuntur ex quam nam rerum quos architecto perspiciatis tempore eaque impedit maxime,
+                             at nesciunt voluptatem aperiam repellendus minima quae!</label>
+                        </div>
+                    </div>
+                </div>
+                <div className='register'>
+                    <div className='register-banner'>
+                        <img src={banner}/>
+                    </div>
+                    <div className='register-label'>
+                        <div className='register-title'>
+                        <h1>Cadastre-se e faça parte dessa comunidade</h1>
+                        </div>
+                        <button></button>
                     </div>
                 </div>
             </div>
