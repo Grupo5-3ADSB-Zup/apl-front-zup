ab885bc30aa6004e4d6a6936ec724800c0324fe2
Eduardo Oliveira <99743355+Eduardooliveira95@users.noreply.github.com>
2023-06-10 17:12:14 -0300

    Correção busca do Axios

--- client/services/services.js
+++ client/services/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({baseURL: "https://apl-front-zup-teste-p3ioltg72-vercel-zup.vercel.app/"})
+const api = axios.create({baseURL: "https://api-zup.azurewebsites.net/"})
 
 export default api;
