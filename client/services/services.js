6483def5951426ef65165f2b2b17a79e9a43da88
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-08-12 00:52:12 -0300

    Atualização URL apontamento back-end

--- client/services/services.js
+++ client/services/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({baseURL: "https://api-zup.azurewebsites.net/"})
+const api = axios.create({baseURL: "https://apl-back-end-zup.azurewebsites.net/"})
 
 export default api;
